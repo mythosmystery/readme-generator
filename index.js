1f324674d0c94b51f844644c39c4292d22384931
Hunter Barton <axiysfire@gmail.com>
2021-04-22 19:23:50 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const inquirer = require("inquirer");
+
+inquirer
+   .prompt([
+      {
+         type: "input",
+         message: "What is your user name?",
+         name: "username",
+      },
+   ])
+   .then((response) => {
+      console.log(response.username);
+   });
