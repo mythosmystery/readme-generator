bd830369232e49cc7338af25f5cd17e07c73c236
Hunter Barton <axiysfire@gmail.com>
2021-04-22 19:53:31 -0500

    added packages and user input questions

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your Github username?",
+    "What is the title of your project?",
+    "Please enter a description of your project",
+    "Enter installation instructions for your project",
+    "Enter usage information about your project",
+    "Enter the guidelines for contributing to your project",
+    "Enter guidelines for how to test your project",
+    "What is your email?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
