98fd6f6bdd1af4bf4c6ab9c5d69fa442c415a67f
Hunter Barton <axiysfire@gmail.com>
2021-04-24 11:06:37 -0500

    added markdown generation

--- index.js
+++ index.js
@@ -2,23 +2,56 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+const README_LINK = "readme.md";
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is your Github username?",
-    "What is the title of your project?",
-    "Please enter a description of your project",
-    "Enter installation instructions for your project",
-    "Enter usage information about your project",
-    "Enter the guidelines for contributing to your project",
-    "Enter guidelines for how to test your project",
-    "What is your email?"
+    "What is your name?",
+   "What is your Github username?",
+   "What is the title of your project?",
+   "Please enter a description of your project",
+   "Enter installation instructions for your project",
+   "Enter usage information about your project",
+   "Enter the guidelines for contributing to your project",
+   "Enter guidelines for how to test your project",
+   "What is your email?",
 ];
+const names = ["name", "username", "title", "description", "install", "usage", "contrib", "testing", "email"];
+const licenses = 
+   {
+      type: "list",
+      message: "Choose the license for your project:",
+      choices: ["Mozilla Public License", "Apache", "MIT", "Boost Software License", "Unlicense"],
+      name: "license",
+   };
 
+function getUserInput() {
+    const prompts = [];   
+   for (let i = 0; i < questions.length; i++) {
+      prompts.push({
+         type: "input",
+         message: questions[i],
+         name: names[i],
+      });
+   }
+   prompts.push(licenses);
+   inquirer.prompt(prompts).then((res) => {      
+      console.log(res);         
+      generateReadme(res);
+   });
+}
+function generateReadme(data) {
+   const readme = generateMarkdown(data);
+   console.log(readme);
+   writeFileAsync(README_LINK, readme)
+   .then(() => console.log("write ok"))
+   .catch((err) => console.error(err));
+}
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+const writeFileAsync = util.promisify(fs.writeFile);
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+   getUserInput();
+}
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+   if (license == "Mozilla Public License") {
+      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+   } else if (license == "Apache") {
+      return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+   } else if (license == "MIT") {
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+   } else if (license == "Boost Software License") {
+      return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
+   } else if (license == "Unlicense") {
+      return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+   }
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
@@ -12,9 +23,41 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+   return `    
+    ##Title: ${data.title}
+    ${renderLicenseBadge(data.license)}
+    ### Project Created By: ${data.name}
+    ### Description: ${data.description}
+    **
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license) 
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    **
+    ## Installation 
+    ${data.install}
 
-`;
+    ## Usage
+    ${data.usage}
+
+    ## License 
+    ${renderLicenseBadge(data.license)}
+
+    ## Contributing
+    If you would like to add to this project, you can [follow me on GitHub](https://github.com/${data.username}).  
+
+    ## Tests
+    ${data.testing}
+
+    ## Questions:
+    If you have any questions about this project, you can reach me [on GitHub](https://github.com/${
+       data.username
+    }) or via email at ${data.email}.
+  `;
 }
 
 module.exports = generateMarkdown;
